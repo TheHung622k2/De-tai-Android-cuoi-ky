015bbd0485952f9ff40eb756d2d9cf4a052467ca
The Hung <thehung622k2@gmail.com>
2023-12-06 14:21:27 +0700

    Cap nhat phan yeu cau quyen truy cap tu nguoi dung

--- QRScanner/app/src/main/java/vn/com/phamthehung/qrscanner/MainActivity.java
+++ QRScanner/app/src/main/java/vn/com/phamthehung/qrscanner/MainActivity.java
@@ -1,6 +1,13 @@
 package vn.com.phamthehung.qrscanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
@@ -8,11 +15,26 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Size;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
 
+import java.io.Reader;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -65,11 +87,102 @@ public class MainActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(this));
     }
 
+    // Yeu cau quyen truy cap tu nguoi dung
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (requestCode == 101 && grantResults.length > 0) {
+            ProcessCameraProvider processCameraProvider = null;
+            try {
+                processCameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            bindpreview(processCameraProvider);
+        }
+
+
+
+
+    }
+
+    private void bindpreview(ProcessCameraProvider processCameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(
+                CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        ImageCapture imageCapture = new ImageCapture.Builder().build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(cameraExecutor, analyzer);
+        processCameraProvider.unbindAll();
+        processCameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+
+
+
+    }
+
 
     // Image analyzer class
-    public class MyImageAnalyzer {
+    public class MyImageAnalyzer implements ImageAnalysis.Analyzer{
+        private FragmentManager fragmentManager;
+        private BottomDialog bottomDialog;
         public MyImageAnalyzer(FragmentManager supportFragmentManager) {
+            this.fragmentManager = supportFragmentManager;
+            bottomDialog = new BottomDialog();
+        }
 
+        // Quet ma vach
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+            ScanBarCode(image);
+        }
+
+        private void ScanBarCode(ImageProxy image) {
+            @SuppressLint("UnsafeOptInUsageError") Image image1 = image.getImage();
+
+            assert image1 != null;
+            InputImage inputImage = InputImage.fromMediaImage(image1, image.getImageInfo().getRotationDegrees());
+
+            BarcodeScannerOptions scannerOptions = new BarcodeScannerOptions.Builder()
+                    .setBarcodeFormats(
+                            Barcode.FORMAT_QR_CODE,
+                            Barcode.FORMAT_AZTEC
+                    ).build();
+
+            BarcodeScanner scanner = BarcodeScanning.getClient(scannerOptions);
+            Task<List<Barcode>> result = scanner.process(inputImage)
+                    .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                        @Override
+                        public void onSuccess(List<Barcode> barcodes) {
+                            ReaderBarCodeData(barcodes);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Neu doc Barcode that bai
+                            Toast.makeText(MainActivity.this, "Doc ma vach that bai!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
+                        @Override
+                        public void onComplete(@NonNull Task<List<Barcode>> task) {
+                            image.close();
+                        }
+                    });
+
+        }
+
+        private void ReaderBarCodeData(List<Barcode> barcodes) {
+            
         }
     }
 }
